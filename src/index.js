a2eeeea4e59245d4982b7ac875fce001c5bedc9d
gabin <bini020408@naver.com>
2025-08-21 16:46:43 +0900

    배포 준비

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { store } from './components/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Provider store={store}>
       <React.StrictMode>
         <App />
